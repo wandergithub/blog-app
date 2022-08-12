54a47744e8e16e62b7e2282a7b0b26570fb0dc3a
wandergithub <wandergonzalez0@hotmail.com>
2022-08-12 13:03:51 -0400

    Create user specs

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'User validation' do
+    it 'Is invalid when :name is not provided' do
+      expect(User.new(posts_counter: 0)).to be_invalid
+    end
+
+    it 'Is valid when :name is provided' do
+      expect(User.new(name: 'carlos', posts_counter: 0)).to be_valid
+    end
+
+    it 'Is invalid when :posts_counter is not an integer' do
+      expect(User.new(name: 'carlos', posts_counter: 'string')).to be_invalid
+    end
+
+    it 'Is invalid when :posts_counter is less than 0' do
+      expect(User.new(name: 'carlos', posts_counter: -2)).to be_invalid
+    end
+  end
+end
