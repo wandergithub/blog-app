cf005f01889da92dadd705573172bf70ecfb05ac
wandergithub <wandergonzalez0@hotmail.com>
2022-08-12 00:40:54 -0400

    Create like class  method

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,4 +3,9 @@ require_relative 'application_record'
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  # A method that updates the likes counter for a post.
+  def update_likes_counter_in_post
+    post.likes_counter.nil? ? post.update(likes_counter: 1) : post.update(likes_counter: post.likes_counter + 1)
+  end
 end
