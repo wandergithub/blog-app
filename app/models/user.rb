63155a55f84f8e621f24725c2c538c1b82e5cfe3
wandergithub <wandergonzalez0@hotmail.com>
2022-08-12 11:59:09 -0400

    add Post model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ require_relative 'application_record'
 
 class User < ApplicationRecord
   validates :name, presence: true
-  validates :posts_counter, comparison: { grater_than_or_equal_to: 0 }
+  validates :posts_counter, numericality: { only_integer: true }, comparison: { grater_than_or_equal_to: 0 }
 
   has_many :posts, class_name: 'Post', foreign_key: 'author_id'
   has_many :comments, class_name: 'Comment', foreign_key: 'author_id'
