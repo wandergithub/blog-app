72d6d537cbd84ff7163d37467e0ef292ba3a0267
wandergithub <wandergonzalez0@hotmail.com>
2022-08-12 00:28:56 -0400

    write Comment method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,9 @@ require_relative 'application_record'
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  # A method that updates the comments counter for a post.
+  def  update_comments_counter_in_post
+    post.comments_counter.nil? ? post.update(comments_counter: 1) : post.update(comments_counter: post.comments_counter + 1)
+  end
 end
